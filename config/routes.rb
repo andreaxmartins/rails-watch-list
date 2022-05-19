e2d2e62415a2008ec4d227454f37204bee872931
Andrea Martins <andreaxmartins@gmail.com>
2022-05-19 16:43:17 +0200

    Adds bookmarks routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :lists, except: %i[edit update]
+  root to: 'lists#index'
+  resources :lists, except: %i[edit update] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: %i[destroy]
 end
